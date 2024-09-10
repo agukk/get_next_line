016f9e07d5237d485b7e45a1a337e3e0a1213443
Kenta Kato <01keny01@gmail.com>
2024-09-10 22:02:06 +0900

    solve a question

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,65 @@
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 07:59:06 by kentakato         #+#    #+#             */
-/*   Updated: 2024/09/10 18:48:28 by kentakato        ###   ########.fr       */
+/*   Updated: 2024/09/10 21:57:25 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int ft_getc(int fd)
+{
+    ssize_t n = 0; 
+    unsigned char buf[BUF_SIZE];
+    unsigned char *bufp;
+    
+    if (n == 0)
+    {
+        n = read(fd, buf, BUF_SIZE);
+        if (n == READ_ERROR)
+        {
+            return READ_ERROR;
+        }
+        bufp = buf;
+    }
+    
+    return (--n >= 0) ? *bufp++ : EOF;
+}
+
+int ft_putc(t_string *str, char c)
+{
+    if (str->capacity < str->len + 1)
+    {
+        str->str = (char *)malloc(sizeof(char) * ());
+    }
+    str->str[str->len] = c;
+    str->len++;
+    return 0;
+}
+
 char *get_next_line(int fd)
 {
-    char *line;
-    ssize_t byte;
-    size_t i = 0;
-    static char buf[BUF_SIZE];
+    t_string ret;
+    char c;
 
-    if (fd < 0 || BUF_SIZE <= 0)
-        return NULL;
-
-    line = (char *)malloc(sizeof(char) * (BUF_SIZE + 1));
-    if (!line)
-        return NULL;
-
-    byte = read(fd, line, BUF_SIZE);
-    buf[byte] = '\0';
+    ret.str = NULL;
+    ret.len = 0;
+    ret.capacity = 0;
     
-    return line;
+    while (1)
+    {
+        c = ft_getc(fd);
+        if (c == EOF)
+            break;
+        ft_putc(&ret, c);
+        if (c == '\n')
+            break;
+    }
+    if (ret.len > 0)
+    {
+        ft_putc(&ret, '\0');
+    }
+    return ret.str;
 }
 
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 07:59:08 by kentakato         #+#    #+#             */
-/*   Updated: 2024/09/05 18:48:28 by kentakato        ###   ########.fr       */
+/*   Updated: 2024/09/10 21:43:50 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 #define GET_NEXT_LINE_H
 
 #define BUF_SIZE 1
+#define READ_ERROR -1
 
 #include <stdio.h> // 課題終わったら消す
 #include <unistd.h>
 #include <fcntl.h> // 課題終わったら消す
 #include <stdlib.h>
+
+typedef struct s_string
+{
+    char *str;
+    size_t len;
+    size_t capacity;
+} t_string;
+
 
 #endif 
